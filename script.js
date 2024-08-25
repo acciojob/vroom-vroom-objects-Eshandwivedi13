ba315a9cb530d48798bda40144b7177d67201f82
Eshan Dwivedi <79198616+Eshandwivedi13@users.noreply.github.com>
2024-08-25 19:27:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,19 @@
 // Complete the js code
-function Car(make, model) {}
+function Car(make, model) {
+	this.make = make;
+	this.model = model;
+	this.getMakeModel = function(){//attaching this method to object
+		return this.make + " " + this.model;
+	}
+}
 
-function SportsCar(make, model, topSpeed) {}
+function SportsCar(make, model, topSpeed) {
+	Car.call(this, make, model);//call will take 'this' newMemory and will call Car function with it only
+	this.topSpeed = topSpeed;
+	this.getTopSpeed = () => {
+		return this.topSpeed;
+	}
+}
 
 // Do not change the code below
 window.Car = Car;
